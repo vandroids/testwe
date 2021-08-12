70e9b84951cf84078f3bc41f59f631cad3ab352d
vandroids <vandroids@gmail.com>
2021-08-12 11:09:09 +0500

    Add API key authentication

---
+++ app/src/Entity/User.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @method string getUserIdentifier()
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected int $id;
+
+    /**
+     * @ORM\Column(name="username", type="string", length=255, nullable=false)
+     */
+    protected string $username;
+
+    /**
+     * @ORM\Column(name="key", type="string", length=255, nullable=false)
+     */
+    protected string $key;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+
+    }
+
+    public function getSalt()
+    {
+
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+
+    public function __call($name, $arguments)
+    {
+
+    }
+}
---
+++ app/src/Repository/UserRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+class UserRepository extends ServiceEntityRepository
+{
+    private EntityManagerInterface $manager;
+
+    public function __construct
+    (
+        ManagerRegistry $registry,
+        EntityManagerInterface $manager
+    ) {
+        parent::__construct($registry, User::class);
+        $this->manager = $manager;
+    }
+}
